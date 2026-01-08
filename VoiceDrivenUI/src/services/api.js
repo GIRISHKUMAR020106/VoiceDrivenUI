914c9f62daa52372e0343e6188d8471d285dc814
Prajan Karthik <prjnkrthk@gmail.com>
2026-01-08 20:59:38 +0530

    change base url

--- VoiceDrivenUI/src/services/api.js
+++ VoiceDrivenUI/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Backend base URL
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "http://127.0.0.1:8080";
 
 // Axios instance
 const api = axios.create({
